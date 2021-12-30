6f0dbdd2d9c2c11e0c88d71452bdb4c464e5fc17
Ashish <therobomarket@gmail.com>
2021-12-30 13:33:33 +0530

    setup done

---
+++ build/lib/src/get_data.py
@@ -0,0 +1,31 @@
+## read params
+## process
+## return the dataframe
+
+import os
+
+import pandas as pd
+import yaml
+import pandas
+import argparse
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def get_data(config_path):
+    config = read_params(config_path)
+    # print(config)
+    data_path = config["data_source"]["remote_source"]
+    df = pd.read_csv(data_path)
+    return df
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    # data = get_data(config_path=parsed_args.config)
