809780b31ef531a0b9dcc61cbafc66472a099459
Ashish <therobomarket@gmail.com>
2021-12-30 13:27:25 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version= "0.0.1",
+    description = "its BigMart Sales Prediction package",
+    autthor = "Developer Ashish",
+    packages = find_packages()
+)
