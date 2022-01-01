92e0b4f72421c6f1ba87dc17912ef436b28f0a88
Ashish <therobomarket@gmail.com>
2022-01-01 15:39:41 +0530

    prediction py file updated

--- app.py
+++ app.py
@@ -3,41 +3,17 @@ import os
 import yaml
 import joblib
 import numpy as np
+from prediction_service import prediction
 
-params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    return prediction
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response": response[0]}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Try Again"}
-        return error
 
 
 @app.route('/predict',methods=['POST','GET'])
@@ -45,22 +21,23 @@ def result():
     if request.method == "POST":
         try:
             if request.form:
-                data = [list(dict(request.form).values())]
-                prediction = predict(data)
-                print(prediction)
-                return jsonify(prediction[0])
+                data_req = dict(request.form)
+                print(data_req)
+                response = prediction.form_response(data_req)
+                print(response)
+                return jsonify(response)
 
             elif request.json:
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 print(response)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
             error = {"error": "Something went wrong!! Try Again"}
-            return jsonify(error)
+            return jsonify(e)
     else:
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8278, debug= True)
+    app.run(host="0.0.0.0", port=8270, debug= True)
---
+++ prediction_service/prediction.py
@@ -0,0 +1,92 @@
+import yaml
+import os
+import json
+import joblib
+import numpy as np
+
+params_path = "params.yaml"
+schema_path = os.path.join("prediction_service","schema_in.json")
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+class NotInCols(Exception):
+    def __init__(self, message="Not in columns"):
+        self.message = message
+        super().__init__(self.message)
+
+def read_params(config_path=params_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction
+
+   # try:
+    #    if 3 <= prediction <= 8:
+       #     print(prediction)
+        #    return prediction
+        #else:
+         #   raise NotInRange
+    #except NotInRange:
+     #   return "Unexpected result"
+
+def get_schema(schema_path=schema_path):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
+
+
+def validate_input(dict_request):
+    def _validate_cols(col):
+        schema = get_schema()
+        actual_cols = schema.keys()
+        if col not in actual_cols:
+            raise NotInCols
+
+    def _validate_values(col, val):
+        schema = get_schema()
+        if not (schema[col]["min"] <= dict_request[col] <=["max"]):
+            raise NotInRange
+
+
+    #for col, val in dict_request.items():
+       # _validate_cols(col)
+       # _validate_values(col, val)
+    return True
+
+
+
+def form_response(dict_request):
+    try:
+        if validate_input(dict_request):
+            data = [list(dict_request.values())]
+            response = predict(data)
+            response = {"response": response[0]}
+            return response
+
+    except Exception as e:
+        response = {"the_expected_range": get_schema(), "response": str(e)}
+        return response
+
+def api_response(dict_request):
+    try:
+        if validate_input(dict_request):
+            data = np.array([list(dict(dict_request.form).values())])
+            response = predict(data)
+            print(response)
+            response = {"response": response}
+            return response
+
+    except Exception as e:
+        response = {"the_expected_range": get_schema(), "response": str(e)}
+        return response
+
